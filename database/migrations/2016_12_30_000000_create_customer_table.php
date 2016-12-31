bca55f0cb7a6a1141d24ed906aa695e154d9c77b
katjapitschel <katja.pitschel@adhoc.de>
2017-01-01 00:35:53 +0100

    issue-1 create customer table and add test customer seeder

--- database/migrations/2016_12_30_000000_create_customer_table.php
+++ database/migrations/2016_12_30_000000_create_customer_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCustomerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('custumers', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('company')->unique();
             $table->string('street');
@@ -35,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('custumers');
+        Schema::drop('customers');
     }
 }
